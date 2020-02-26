96576a11939bb5583cba1731b627d33fa3fadd34
Oleg Lysenko <lisenko.oleg@playster.com>
2020-02-26 18:15:07 +0200

    rollback

--- document_picker/ios/Runner/AppDelegate.swift
+++ document_picker/ios/Runner/AppDelegate.swift
@@ -12,19 +12,19 @@ import Flutter
     let rootViewController = window?.rootViewController as! FlutterViewController
     let methodChannel = FlutterMethodChannel(name: channelName, binaryMessenger: rootViewController.binaryMessenger)
     
-//    methodChannel.setMethodCallHandler {(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-//      if (call.method == "start") {
-//        let controller = DocumentPickerViewController(result: result)
-//        rootViewController.present(controller, animated: true, completion: nil)
-//      }
-//    }
-    
     methodChannel.setMethodCallHandler {(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
       if (call.method == "start") {
-        let controller = DocumentBrowserViewController(result: result)
+        let controller = DocumentPickerViewController(result: result)
         rootViewController.present(controller, animated: true, completion: nil)
       }
     }
+    
+//    methodChannel.setMethodCallHandler {(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+//      if (call.method == "start") {
+//        let controller = DocumentBrowserViewController(result: result)
+//        rootViewController.present(controller, animated: true, completion: nil)
+//      }
+//    }
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
